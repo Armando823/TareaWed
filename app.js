7b24a262dd72d9c1f7156be1a3f5abb278417ba4
Armando823 <rosalestimoteomauricio835@gmail.com>
2026-04-22 14:12:32 -0500

    1

--- app.js
+++ app.js
@@ -2,24 +2,42 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+// CONFIGURACIÓN
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+// MIDDLEWARES
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// RUTAS
-app.get("/", (req, res) => res.render("index"));
-app.get("/perfil", (req, res) => res.render("perfil"));
-app.get("/album", (req, res) => res.render("album"));
-app.get("/contactos", (req, res) => res.render("contactos"));
-app.get("/favoritos", (req, res) => res.render("favoritos"));
-
-app.listen(3000, () => {
-  console.log("Servidor en http://localhost:3000");
+// RUTAS PRINCIPALES
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
+app.get("/perfil", (req, res) => {
+  res.render("perfil");
+});
+
+app.get("/album", (req, res) => {
+  res.render("album");
+});
+
+app.get("/contactos", (req, res) => {
+  res.render("contactos");
+});
+
+app.get("/favoritos", (req, res) => {
+  res.render("favoritos");
+});
+
+// API AJAX
 app.get("/api/contactos", (req, res) => {
   res.json(["Juan", "Maria", "Carlos"]);
+});
+
+// SERVIDOR
+app.listen(3000, () => {
+  console.log("Servidor listo en http://localhost:3000");
 });
